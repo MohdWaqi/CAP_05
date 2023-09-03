7ebe75c49d26f319684d7cbb49a6005eb0acbc57
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2023-09-03 22:37:30 +0530

    update submission functionality

--- DOM/Employee Records/record.js
+++ DOM/Employee Records/record.js
@@ -10,7 +10,7 @@ const tableBody = document.querySelector("tbody");
 
 formData.addEventListener("submit", function (event) {
   event.preventDefault();
-  
+
   let gotData = {
     name: employeeName.value,
     id: employeeId.value,
@@ -19,44 +19,43 @@ formData.addEventListener("submit", function (event) {
     mail: employeeEmail.value,
     contact: employeeMobile.value,
   };
-    let tableRow = document.createElement("tr");
-    let tdName = document.createElement("td");
-    let tdId = document.createElement("td");
-    let tdDept = document.createElement("td");
-    let tdEmail = document.createElement("td");
-    let tdExperience = document.createElement("td");
-    let tdContact = document.createElement("td");
-    let tdRole = document.createElement("td");
-    let tdDelete = document.createElement("td");
-    
+  let tableRow = document.createElement("tr");
+  let tdName = document.createElement("td");
+  let tdId = document.createElement("td");
+  let tdDept = document.createElement("td");
+  let tdEmail = document.createElement("td");
+  let tdExperience = document.createElement("td");
+  let tdContact = document.createElement("td");
+  let tdRole = document.createElement("td");
+  let tdDelete = document.createElement("td");
 
-    if (gotData.experience <= 1) {
-      tdRole.innerText = "Fresher";
-    } else if (gotData.experience >= 2 && gotData.experience <= 5) {
-      tdRole.innerText = "Junior";
-    } else {
-      tdRole.innerText = "Senior";
-    }
+  if (gotData.experience <= 1) {
+    tdRole.innerText = "Fresher";
+  } else if (gotData.experience >= 2 && gotData.experience <= 5) {
+    tdRole.innerText = "Junior";
+  } else {
+    tdRole.innerText = "Senior";
+  }
 
-    tdName.innerText = gotData.name;
-    tdId.innerText = gotData.id;
-    tdDept.innerText = gotData.department;
-    tdEmail.innerText = gotData.mail;
-    tdExperience.innerText = gotData.experience;
-    tdContact.innerText = gotData.contact;
+  tdName.innerText = gotData.name;
+  tdId.innerText = gotData.id;
+  tdDept.innerText = gotData.department;
+  tdEmail.innerText = gotData.mail;
+  tdExperience.innerText = gotData.experience;
+  tdContact.innerText = gotData.contact;
 
-    tdDelete.innerHTML = "<button class='deleteBtn'>Delete</button>";
-    tableRow.append(
-      tdName,
-      tdId,
-      tdDept,
-      tdExperience,
-      tdEmail,
-      tdContact,
-      tdRole,
-      tdDelete
-    );
-    tableBody.append(tableRow);
+  tdDelete.innerHTML = "<button class='deleteBtn'>Delete</button>";
+  tableRow.append(
+    tdName,
+    tdId,
+    tdDept,
+    tdExperience,
+    tdEmail,
+    tdContact,
+    tdRole,
+    tdDelete
+  );
+  tableBody.append(tableRow);
 
   let tableEL = document.querySelector("table");
 
@@ -66,13 +65,20 @@ formData.addEventListener("submit", function (event) {
     element.style.backgroundColor = "red";
   }
 
-  tableEL.addEventListener("click",function(e){
-    if(!e.target.classList.contains("deleteBtn")){
-      console.log(e.target.classList)
+  tableEL.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("deleteBtn")) {
+      console.log(e.target.classList);
       return;
     }
-    console.log(e.target.classList)
+    console.log(e.target.classList);
     const btn = e.target;
     btn.closest("tr").remove();
   });
+  employeeName.value = "";
+  employeeId.value = "";
+  employeeDept.value = "";
+  employeeExperience.value = "";
+  employeeEmail.value = "";
+  employeeMobile.value = "";
+  tableBody.value = "";
 });
---
+++ DOM/Hospital- Doctor Records/index.js
@@ -0,0 +1,82 @@
+// fill in javascript code here
+// fill in javascript code here
+const formData = document.querySelector("form");
+const doctorName = document.getElementById("name");
+const doctorId = document.getElementById("docID");
+const doctorDept = document.getElementById("dept");
+const doctorExperience = document.getElementById("exp");
+const doctorEmail = document.getElementById("email");
+const doctorMobile = document.getElementById("mbl");
+const tableBody = document.querySelector("tbody");
+
+formData.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let allDoctors = []
+  allDoctors.push({
+    name: doctorName.value,
+    id: doctorId.value,
+    department: doctorDept.value,
+    experience: doctorExperience.value,
+    mail: doctorEmail.value,
+    contact: doctorMobile.value,
+  });
+  allDoctors.forEach(function (element){
+    let tableRow = document.createElement("tr");
+    let tdName = document.createElement("td");
+    let tdId = document.createElement("td");
+    let tdDept = document.createElement("td");
+    let tdEmail = document.createElement("td");
+    let tdExperience = document.createElement("td");
+    let tdContact = document.createElement("td");
+    let tdRole = document.createElement("td");
+    let tdDelete = document.createElement("td");
+    
+
+    if (element.experience <= 1) {
+      tdRole.innerText = "Trainee";
+    } else if (element.experience >= 2 && element.experience <= 5) {
+      tdRole.innerText = "Junior";
+    } else {
+      tdRole.innerText = "Senior";
+    }
+
+    tdName.innerText = element.name;
+    tdId.innerText = element.id;
+    tdDept.innerText = element.department;
+    tdEmail.innerText = element.mail;
+    tdExperience.innerText = element.experience;
+    tdContact.innerText = element.contact;
+
+    tdDelete.innerHTML = "<button class='deleteBtn'>Delete</button>";
+    tableRow.append(
+      tdName,
+      tdId,
+      tdDept,
+      tdExperience,
+      tdEmail,
+      tdContact,
+      tdRole,
+      tdDelete
+    );
+    tableBody.append(tableRow);
+    let deleteButton = document.querySelectorAll("td > button");
+
+  for (let element of deleteButton) {
+    element.style.backgroundColor = "red";
+  }
+  });
+    
+  let tableEL = document.querySelector("table");
+
+  tableEL.addEventListener("click",function(e){
+    if(!e.target.classList.contains("deleteBtn")){
+      console.log(e.target.classList)
+      return;
+    }
+    console.log(e.target.classList)
+    const btn = e.target;
+    btn.closest("tr").remove();
+  });
+});
+
+
