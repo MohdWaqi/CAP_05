11390c70ebd013f8ce5f1acc87460a2f19403f69
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2023-10-08 00:35:43 +0530

    handling errors

--- Fetch/Omdb_App/index.js
+++ Fetch/Omdb_App/index.js
@@ -1,28 +1,41 @@
 let apiKey = "8992ed0b";
 let enteredMovie = document.querySelector("input");
 let searchButton = document.querySelector("button");
-let displayArea = document.querySelector("#navBar+ div");
+let displayArea = document.querySelector("#navBar+ div >div");
+let displayError = document.querySelector("#navBar+ div :last-child");
 searchButton.addEventListener("click", () => {
   displayArea.innerHTML = "";
   let endpoint = `https://www.omdbapi.com/?apikey=${apiKey}&s=${enteredMovie.value}`;
   async function request() {
-    let response = await fetch(endpoint);
-    let fetchedData = await response.json();
-    renderMovies(fetchedData.Search);
+    try {
+      let response = await fetch(endpoint);
+      let fetchedData = await response.json();
+      renderMovies(fetchedData.Search);
+    } catch (error) {
+      displayArea.innerHTML = "";
+      displayError.innerHTML = "";
+      displayError.innerHTML = "<h1>Please Check Your Connection.</h1>";
+    }
   }
   request();
 });
 function renderMovies(data) {
-  data.map((element, index) => {
-    let movieCard = document.createElement("div");
-    let image = document.createElement("img");
-    image.src = element.Poster;
-    let release = document.createElement("h4");
-    release.innerText = `Released: ${element.Year}`;
-    let movieName = document.createElement("h4");
-    movieName.innerText = `${index + 1}. ${element.Title}`;
-    movieCard.append(image, release, movieName);
-    displayArea.append(movieCard);
-    console.log(data);
-  });
+  if (data) {
+    displayError.innerHTML = "";
+    data.map((element, index) => {
+      let movieCard = document.createElement("div");
+      let image = document.createElement("img");
+      image.src = element.Poster;
+      let release = document.createElement("h4");
+      release.innerText = `Released: ${element.Year}`;
+      let movieName = document.createElement("h4");
+      movieName.innerText = `${index + 1}. ${element.Title}`;
+      movieCard.append(image, release, movieName);
+      displayArea.append(movieCard);
+      console.log(data);
+    });
+  } else {
+    displayError.innerHTML = "";
+    displayError.innerHTML = "<h1>No Movie Found...</h1>";
+  }
 }
