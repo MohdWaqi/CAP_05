1511c2c57b4d2487933453415e3dd0dab9f41685
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2023-10-14 15:40:40 +0530

    remove duplictation

--- React/counter/src/main.jsx
+++ React/counter/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { useState } from 'react'
+
 
 const Counter = ()=>{
   const [count, setCount] = useState(0)
